e6a698d32bc9d077f54329ceeaf2490d1ada57bd
dansam100 <dansam100@gmail.com>
2010-08-10 18:43:53 -0400

    removing binaries

---
+++ MultiSampler/MultiSampler/SampleBox.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MultiSampler
+{
+    public delegate void AverageAcquiredHandler(double[] values, double output);
+
+    public class SampleBox
+    {
+        private Stack<double> contents;
+
+        public int AveragingDepth { get; set; }
+        public int Size { get; set; }
+        public event AverageAcquiredHandler OnAverageAcquired;
+
+        /// <summary>
+        /// ctor
+        /// </summary>
+        /// <param name="size">size of samples to collect; should be at least depth+1</param>
+        /// <param name="depth">how deep should the averaging be?</param>
+        public SampleBox(int size, int depth)
+        {
+            this.AveragingDepth = depth;
+            if (size >= depth + 1)
+            {
+                this.Size = size;
+                contents = new Stack<double>(Size);
+            }
+            else throw new Exception("Size must be greater than depth!");
+        }
+
+        public void Add(double sample)
+        {
+            contents.Push(sample);
+        }
+
+        /// <summary>
+        /// Perform an average of the current values in the samplebox
+        /// </summary>
+        /// <param name="indices"></param>
+        /// <returns></returns>
+        internal double[] PerformAverage(params int[] indices)
+        {
+            double[] results;
+            if (indices.Length > 0)
+            {
+                results = new double[indices.Length];
+                int j = 0;
+                double[] array = contents.ToArray<double>();
+                foreach(int i in indices)
+                {
+                    int which = (i % 2 != 0) ? -1 : 1;
+                    if( (i+which) < array.Length){
+                        results[j] = 0.5*(array[i] + array(i+which));
+                    }
+                }
+                return results;
+            }
+            else
+            {
+                results = new double[this.Size];
+                double[] array = contents.ToArray<double>();
+                int j = 0;
+                foreach (int i in this.contents)
+                {
+                    if (i < array.Length - 1)
+                    {
+                        results[j] = 0.5 * (contents.Select<double>(i) + contents.Select<double>(i + which));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MultiSampler/MultiSampler/TaskItem.cs
@@ -0,0 +1,180 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NationalInstruments.DAQmx;
+using System.Net.Sockets;
+using System.ComponentModel;
+
+namespace MultiSampler
+{
+    public delegate void DataReadEventHandler(double data);
+    public delegate void CharReadEventHandler(byte[] data);
+    
+    public abstract class TaskItem
+    {
+        public string Name{ get; set; }
+        protected Task myTask;
+        protected TcpClient connection;
+        protected NetworkStream stream;
+        
+        protected const string targetIP = "localhost";
+        protected const int port = 9191;
+
+        public event DataReadEventHandler OnEventRead;
+        public event CharReadEventHandler OnCharRead;
+
+        public TaskItem(string name)
+        {
+            this.Name = name;
+            this.connection = new TcpClient();
+
+            this.OnEventRead += new DataReadEventHandler(TaskItem_OnEventRead);
+            this.OnCharRead += new CharReadEventHandler(TaskItem_OnCharRead);
+        }
+
+        public abstract void DoWork(BackgroundWorker worker);
+        public abstract void Test(BackgroundWorker worder);
+
+        protected void Connect()
+        {
+            try
+            {
+                if(this.connection.Connected)
+                {
+                    this.connection.Close();
+                }
+                connection.Connect(targetIP, port);
+                stream = connection.GetStream();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unable to connect to server.\nReason: {0}", e);
+            }
+        }
+
+        protected virtual void TaskItem_OnEventRead(double data)
+        {
+            try
+            {
+                if (connection.Connected){
+                    NetworkStream stream = connection.GetStream();
+                    ASCIIEncoding ascii = new ASCIIEncoding();
+                    byte[] buff = new byte[128];
+                    try
+                    {
+                        //string outval = string.Format("{0:0.00}\0", data);
+                        string outval = string.Format("{0}\0", (int)data);
+                        buff = ascii.GetBytes(outval);
+                        stream.Write(buff, 0, outval.Length);
+                        stream.Flush();
+                    }
+                    catch (Exception e){    Console.WriteLine(e);   }
+                }
+                else Connect();
+            }
+            catch (Exception e){
+                Console.WriteLine("Unable to transmit data to server!\nReason: {0}", e);
+            }
+        }
+
+        public void TaskItem_OnCharRead(byte[] data)
+        {
+            try
+            {
+                if (connection.Connected)
+                {
+                    try
+                    {
+                        stream.Write(data, 0, 1);
+                        stream.Flush();
+                    }
+                    catch (Exception e){}
+                }
+                else Connect();
+            }
+            catch (Exception e)
+            {
+            }
+        }
+
+        protected void TriggerReadEvent(double data)
+        {
+            lock (this)
+            {
+                if (this.OnEventRead != null)
+                    this.OnEventRead(data);
+            }
+        }
+
+        protected void TriggerReadEvent(byte[] data)
+        {
+            lock (this)
+            {
+                if (this.OnCharRead != null)
+                    this.OnCharRead(data);
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Extension methods for checking data validity.
+    /// </summary>
+    public static class Extensions
+    {
+        public static string ToFormattedString(this double[] array)
+        {
+            string result = "";
+            foreach (double data in array)
+            {
+                result += string.Format("{0},", data);
+            }
+            return result.Substring(0, result.Length - 1);
+        }
+
+        /*
+        public static bool InSampleWindow(this double[] array, double[] other)
+        {
+            int i = 0;
+            double mask = 1000;
+            bool equal = false;
+            if (other != null)
+            {
+                if (array.Length == other.Length)
+                {
+                    while (i < array.Length)
+                    {
+                        double a = array[i] * mask;
+                        double b = other[i] * mask;
+                        
+                        equal = ((int)a == (int)b);
+                        i++;
+                    }
+                }
+            }
+            return equal;
+        }
+        */
+
+        public static bool InSampleWindow(this double[] array, double[] other)
+        {
+            if (array != null && other != null)
+            {
+                if ((Math.Abs(array[0] - other[0])) < 0.001)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+		
+		public static bool LessThan(this double[] array, double value)
+		{
+			foreach(double data in array){
+				if(data > value) return false;
+			}
+			return true;
+		}
+    }
+}
